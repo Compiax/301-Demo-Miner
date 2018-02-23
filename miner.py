94d44bdfd178093045b4df1118038c682b0b1202
Jason van Hattum <jason@compiax.io>
2018-02-23 11:01:08 +0200

    :sparkles: Made a superficial change to the method of socket exiting

--- miner.py
+++ miner.py
@@ -5,28 +5,44 @@ import datetime
 import os
 import http.client
 import socket
+import sys
 
 def getRandomString(n: int):
     return "".join(choice(ascii_uppercase + digits) for _ in range(n)).encode('utf-8')
 
-if __name__ == "__main__":
+sock = None
+
+def run():
+    global sock
+
     difficulty = int(os.environ['difficulty'] if ('difficulty' in os.environ) else 4)
     nodeName = socket.gethostname()
     print("The difficulty is {}".format(difficulty))
-
+    
+    
     while 1:
-        thehash = sha256(getRandomString(25)).hexdigest()
+        string = getRandomString(25)
+        thehash = sha256(string).hexdigest()
 
         if thehash[:difficulty] == ('0'*difficulty):
             body =  {
                 'node': nodeName,
+                    'key': string.decode('utf-8'),
                 'hash': thehash,
                 'time': str(datetime.datetime.now()),
                 'difficulty': str(difficulty)
             }
+
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect(('192.168.2.37', 9700))
+            sock.send((str(body) + '\n').encode())
+            sock.close()
             print(body)
 
-            # conn = http.client.HTTPConnection('192.168.2.37:9700')
-            # conn.request('POST', '/', str(body).encode())
-            # res = conn.getresponse()
-            # print(res.status)
+if __name__ == "__main__":
+    try:
+        run()
+    except KeyboardInterrupt:
+        print("Closed")
+        sock.close()
+        sys.exit(0)
